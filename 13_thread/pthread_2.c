a23841f28d2feaf73b84b4db529148b78b4a0aba
OliverCaoTora <doracaowei0511@gmail.com>
2026-06-16 15:24:46 -0400

    thread 2

---
+++ 13_thread/pthread_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+static void *my_thread_func(void *data)
+{
+    while(1)
+    { 
+        sleep(1); 
+    }
+}
+
+int main(int argc, char **argv)
+{
+    pthread_t tid;
+    int ret;
+
+    ret = pthread_create(&tid, NULL, my_thread_func, NULL);
+    
+    if(ret)
+    {
+        printf("pthread_creat err!\n");
+        return -1;
+    }
+
+    while(1)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
